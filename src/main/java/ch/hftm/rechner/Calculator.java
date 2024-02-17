486266ead681ec98846cfad4cb08647a6b458ad0
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-02-17 09:26:35 +0100

    Made thiongs even worse

--- src/main/java/ch/hftm/rechner/Calculator.java
+++ src/main/java/ch/hftm/rechner/Calculator.java
@@ -6,7 +6,7 @@ package ch.hftm.rechner;
 public class Calculator {
 
 	public double add(double a, double b) {
-		return a + b + b;
+		return a + b + a+ b;
 	}
 
 	public double subtract(double a, double b) {
